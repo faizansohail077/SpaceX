e798b5efbe5e1bb7b0e1c0fa7ce9ccfdc6ff3fc9
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-03 01:00:12 -0500

    spacex

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import LaunchProfile from "./components/LaunchProfile";
 
 const App = () => {
   const [id, setId] = React.useState(42);
-  const handleIdChange = React.useCallback(newId => {
+  const handleIdChange = React.useCallback((newId) => {
     setId(newId);
   }, []);
 
   return (
     <div className="App">
-      <LaunchList handleIdChange={handleIdChange} />
-      <LaunchProfile id={id} />
+      <div className="App1">
+        <LaunchList handleIdChange={handleIdChange} />
+      </div>
+      <div className="App2">
+        <LaunchProfile id={id} />
+      </div>
     </div>
   );
 };
 
 export default App;
-
 
 // import React from 'react';
 // import LaunchList from './components/LaunchList';
